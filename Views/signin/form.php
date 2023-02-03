b392aaa000cb023039378f92ffafbe47ab0717fe
18Nilsou <nils.saadi@etu.univ-amu.fr>
2023-02-03 18:03:33 +0100

    formulaire de connexion

---
+++ Views/signin/form.php
@@ -0,0 +1,8 @@
+<form class="signForm" method="post" action="/signin/connect">
+    <label for="id">Pseudo :</label>
+    <input type="text" placeholder="Rentrez pseudo" name="id" class="input" required>
+    </div>
+    <label for="password">Mot de passe :</label>
+    <input type="password" placeholder="Rentrez votre mot de passe" class="input" name="password" required>
+    <input id="submit" type="submit">
+</form>
