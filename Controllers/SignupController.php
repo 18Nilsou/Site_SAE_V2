56ccf890ff2f6c5efd26208bdc0b349744862d48
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2023-02-05 12:10:53 +0100

    Ajout de la redirection en temps que personne connectée si le mot de passe est valide

--- Controllers/SignupController.php
+++ Controllers/SignupController.php
@@ -7,7 +7,10 @@ final class SignupController{
     public function registerAction(Array $A_parametres = null, Array $A_postParams = null):void{
         $A_postParams['password'] = hash('sha512', $A_postParams['password']);
         if(Users::create($A_postParams)){
-            View::show("signin/form");
+            Session::start('user', $A_postParams['id']);
+            header('Location: /home');
+            exit;
         }
+        header('Location: /signin');
     }
 }
