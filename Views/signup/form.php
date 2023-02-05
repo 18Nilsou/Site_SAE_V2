b73dbb503ec6f372edce397aafa5bbcbd701503d
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2023-02-05 12:06:12 +0100

    Ajout de la vérification de la conformité du mot de passe et confirmation

--- Views/signup/form.php
+++ Views/signup/form.php
@@ -3,11 +3,12 @@
     <label for="id">Pseudo :</label>
     <input class="id-form-input" type="text" placeholder="Votre pseudo" name="id" required>
     <label for="id">Email :</label>
-    <input class="id-form-input" type="text" placeholder="Votre email" name="email" title="Entrez un email valide" required>
+    <input class="id-form-input" type="email" placeholder="Votre email" name="email" title="Entrez un email valide" required>
     <label for="password">Mot de passe :</label>
-    <input class="id-form-input" type="password" placeholder="Votre mot de passe" name="password" title="Au moins 12 caractères, un chiffre, une lettre majuscule, une minuscule et un caractère spécial" required >
-    <label for="password">Confirmation de passe :</label>
-    <input class="id-form-input" type="password" placeholder="Confirmation du mot de passe" title="Confirmation du mot de passe" required >
+    <input class="id-form-input" id="password" type="password" placeholder="Votre mot de passe" name="password" title="Au moins 12 caractères, un chiffre, une lettre majuscule, une minuscule et un caractère spécial" minlength="12" required >
+    <label for="passwordConfirm">Confirmation de passe :</label>
+    <input class="id-form-input" id="password_confirm" type="password" placeholder="Confirmation du mot de passe" title="Confirmation du mot de passe" required >
     <p class="redirections-sign">Déjà inscris ? <a href="/signin">Connectez vous !</a></p>
     <input class="submit" type="submit">
 </form>
+<script type='text/javascript' src='/static/js/password-check.js'></script>
---
+++ static/js/password-check.js
@@ -0,0 +1,23 @@
+document.querySelector('.submit').addEventListener('click', function(event) {
+    const password = document.querySelector('#password');
+    const passwordConfirm = document.querySelector('#password_confirm');
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_+=[\]{};':"\\|,.<>\/?])/;
+
+    if (password.value.length < 12) {
+        password.setCustomValidity("Votre mot de passe doit comporter au moins 12 caractères");
+    } else {
+        password.setCustomValidity("");
+    }
+
+    if (!regex.test(password.value)) {
+        password.setCustomValidity("Votre mot de passe doit comporter au moins une majuscule, une minuscule, un chiffre et un symbole");
+    } else {
+        password.setCustomValidity("");
+    }
+
+    if (password.value !== passwordConfirm.value) {
+        passwordConfirm.setCustomValidity("Les mots de passe ne correspondent pas");
+    } else {
+        passwordConfirm.setCustomValidity("");
+    }
+});
