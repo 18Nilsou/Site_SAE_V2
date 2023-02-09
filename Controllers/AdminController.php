067c29c3c0a540717370b5830e217c3a7c8b20ed
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2023-02-10 00:26:27 +0100

    Vérification que l'utilisateur est un admin pour accéder à la page d'administration

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -4,6 +4,10 @@ final class AdminController
 {
     public function defaultAction(): void
     {
+        if(Session::getSession()['status'] != 'admin') {
+            header("Location: /home");
+            exit;
+        }
         View::show("admin/admin");
     }
 }
