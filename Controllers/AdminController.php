dde93b2afefa1a6b544bcacef3d80243505c6a9c
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2023-02-12 22:24:24 +0100

    Création des rooms

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -31,4 +31,18 @@ final class AdminController
         View::show("admin/admin-nav");
         View::show("admin/users");
     }
+
+    public function createroomAction(Array $A_parametres = null, Array $A_postParams = null): void
+    {
+        if(Session::getSession()['status'] != 'admin') {
+            header("Location: /home");
+            exit;
+        }
+        $A_postParams['id'] = Rooms::uniqueId();
+        $A_postParams['password'] = Rooms::passwordRoomGenerator();
+        $A_postParams['started'] = 'false';
+        $A_postParams['admin_id'] = Session::getSession()['id'];
+        Rooms::create($A_postParams);
+        header('Location: /admin/multiplayer');
+    }
 }
--- Views/admin/multiplayer.php
+++ Views/admin/multiplayer.php
@@ -1,3 +1,17 @@
-<section class="tab">
+<?php
+echo '<section class="tab">
     <h2>Gestion des parties multijoueurs</h2>
-</section>
+    <section class="multiplayer-containers">
+        <h3>Vos salons de jeu actifs</h3>
+    </section>
+    <section class="multiplayer-containers">
+        <form id="create-room-form" action="/admin/createroom" method="post">
+            <h3>Créer un salon</h3>
+            <section>
+                <label>Nom du salon </label>
+                <input type="text" name="name" required>
+                <input type="submit">
+            </section>
+        </form>
+    </section>
+</section>';
