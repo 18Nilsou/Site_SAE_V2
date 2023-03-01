d20df5a1df5d2399f4672c604119539fe549e498
18Nilsou <nils.saadi@etu.univ-amu.fr>
2023-03-01 16:52:06 +0100

    id des rooms game et practice

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -9,7 +9,7 @@ final class AdminController
             exit;
         }
 
-        $A_questions = array ("trainning" => Questions::getQuestionArray("A1B2C3"), "play" => Questions::getQuestionArray("D4E5F6"));
+        $A_questions = array ("training" => Questions::getQuestionArray("practice"), "play" => Questions::getQuestionArray("game"));
 
         View::show("admin/admin-nav");
         View::show("admin/solo", $A_questions);
