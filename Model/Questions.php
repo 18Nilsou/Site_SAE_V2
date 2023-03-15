4173deab0395e604706f3056cbddb0f5dc550d95
18Nilsou <nils.saadi@etu.univ-amu.fr>
2023-03-15 19:42:49 +0100

    fonction delete pour les questions

--- Model/Questions.php
+++ Model/Questions.php
@@ -83,9 +83,17 @@ final class Questions extends Model{
 
     public static function addList($A_questions){
         foreach($A_questions as $A_question){
+            self::delete($A_question["order_question"], $A_question["room_id"]);
             Questions::create($A_question);
         }
     }
-        
 
+    public static function delete($I_order, $S_room){
+        $O_con = Connection::initConnection();
+        $S_stmnt = "Delete from rooms where room_id = :room_id and order_question = :order";
+        $O_sth = $O_con->prepare($S_stmnt);
+        $O_sth -> bindValue(":room_id", $S_room, PDO::PARAM_STR);
+        $O_sth -> bindValue(":order", $I_order, PDO::PARAM_INT);
+        $O_sth->execute();
+    }
 }
