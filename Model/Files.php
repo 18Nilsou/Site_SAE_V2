1a350c68b8d7040376a4123aa60892ae6f8bdd44
18Nilsou <nils.saadi@etu.univ-amu.fr>
2023-03-15 10:08:52 +0100

    mise a jour de la fonction télécharger les scores

--- Model/Files.php
+++ Model/Files.php
@@ -3,9 +3,10 @@ final class Files{
     
     public static function score(Array $A_Params): void{
         $O_file = fopen('Files/score.txt', 'w');
-        fwrite($O_file, 'Voici les scores :');
+        fwrite($O_file, 'Voici les scores pour la room '.$A_Params[0]['room'].':');
         foreach($A_Params as $A_data){
-            fwrite($O_file, "\n" . $A_data['user_id'] . " : " . $A_data['score']);
+            $S_text = "\n" .  $A_data['name'] .' '. $A_data['user_id']  .' '. $A_data['lastname']  ." : " . $A_data['score'];
+            fwrite($O_file, $S_text);
         }
         fclose($O_file);
     }
@@ -13,12 +14,13 @@ final class Files{
     public static function download($S_file, $S_dir){
         header('Content-Description: File Transfer');
         header('Content-Type: application/txt');
-        header('Content-Disposition: attachment; filename="' . $S_file . '"');
+            header('Content-Disposition: attachment; filename='.$S_file);
         header('Expires: 0');
         header('Cache-Control: must-revalidate');
         header('Pragma: public');
         header('Content-Length: ' . filesize($S_dir));
-        readfile($S_dir); 
+        readfile($S_dir);
+        exit;
     }
 
 }
