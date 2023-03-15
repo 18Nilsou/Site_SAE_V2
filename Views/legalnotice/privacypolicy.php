69a0e3336847cdf0759772cc22c00420a05ed6d3
18Nilsou <nils.saadi@etu.univ-amu.fr>
2023-03-15 18:04:04 +0100

    footer

---
+++ Views/legalnotice/privacypolicy.php
@@ -0,0 +1,25 @@
+<h1>Politique de confidentialité</h1>
+	<p>Nous attachons une grande importance à la protection de la vie privée de nos utilisateurs et à la sécurité de leurs données personnelles. 
+        Cette politique de confidentialité décrit les informations que nous collectons, comment nous les utilisons et les mesures que nous prenons 
+        pour protéger la vie privée de nos utilisateurs.</p>
+	<h2>Données collectées</h2>
+	<p>Les données collectées comprennent les informations que vous avez fournies lors de votre inscription sur le site, telles que votre nom, prénom,
+         adresse e-mail et pseudo. Nous collectons également les scores et commentaires que vous avez créés sur notre plateforme.</p>
+	<h2>Utilisation des données</h2>
+	<p>Nous collectons ces données pour permettre à nos utilisateurs de créer un profil, de jouer à nos jeux et de suivre leurs scores. 
+        Nous utilisons également ces données pour améliorer notre plateforme et personnaliser l'expérience utilisateur.</p>
+	<h2>Partage des données</h2>
+	<p>Seuls les membres de notre équipe ont accès à ces données, ainsi que les administrateurs des salles que vous rejoignez. 
+        Les administrateurs ne peuvent voir que les scores et commentaires laissés sous la salle, ainsi que votre nom, prénom et pseudo. 
+        Nous ne partageons jamais ces données avec des tiers.</p>
+	<h2>Accès aux données</h2>
+	<p>Vous pouvez accéder à toutes vos données personnelles en vous connectant à votre compte et en allant sur la page "Mon compte". 
+        Vous pouvez également supprimer votre compte et modifier vos commentaires à tout moment.</p>
+	<h2>Sécurité des données</h2>
+	<p>Nous prenons la sécurité de vos données personnelles très au sérieux et avons mis en place des mesures de sécurité appropriées pour les protéger. 
+        Les mots de passe sont stockés en utilisant un algorithme de hachage, et nous utilisons des protocoles de sécurité pour protéger les données 
+        des utilisateurs contre les accès non autorisés.</p>
+	<h2>Utilisation des cookies</h2>
+	<p>Nous utilisons des cookies pour permettre à nos utilisateurs de rester connectés à notre site. Aucune information personnelle n'est stockée dans les cookies.</p>
+	<p>Nous nous réservons le droit de modifier cette politique de confidentialité à tout moment et sans préavis. Nous vous recommandons de vérifier
+         régulièrement cette page pour vous assurer que vous êtes au courant de tout changement.</p>
