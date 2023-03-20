085276f2ecd83707b18a92f6fc96271cac36ef52
18Nilsou <nils.saadi@etu.univ-amu.fr>
2023-03-20 18:55:32 +0100

    correction des bugs liés aux id

--- Model/Retrievepassword.php
+++ Model/Retrievepassword.php
@@ -1,14 +1,14 @@
 <?php
 final class Retrievepassword extends Model{
 
-    public static function sendMail(string $S_mail, int $I_token){
-        $A_params['id'] = $S_mail;
+    public static function sendMail(array $A_user, int $I_token){
         $A_params['token'] = $I_token;
-        $A_params['creation_date'] = date("Y-m-d H:i:s");
+        $A_params['id'] = $A_user["id"];
+        $A_params['creation_date'] = date("d-M-Y H:i");
         if(self::create($A_params)){
             $A_mailContent['subject'] = "Récuperation de mot de passe";
             $A_mailContent['body'] = "Voici votre token valable pendant 10min : ".$I_token;
-            Mailer::sendMail($S_mail, $A_mailContent);
+            Mailer::sendMail($A_user["email"], $A_mailContent);
         }
     }
 
@@ -17,11 +17,11 @@ final class Retrievepassword extends Model{
     }
 
     public static function checkToken(int $I_token, string $S_id){
-        $A_user = self::selectById($S_id);
-        if($A_user['token'] == $I_token){
+        $A_usertoken = self::selectById($S_id);
+        if($A_usertoken['token'] == $I_token){
             $I_date = mktime(date("H"), date("i")-10, date("s"), date("m"), date("d"), date("Y"));
-            $S_date = date("Y-m-d H:i:s",$I_date);
-            if($S_date < $A_user['date']){
+            $S_date = date("d-M-Y H:i",$I_date);
+            if($S_date < $A_usertoken['creation_date']){
                 return true;
             }
             self::deleteByID($S_id);
