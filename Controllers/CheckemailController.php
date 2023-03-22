175df854f293ec39b3d4e4fda399fef7be599e90
18Nilsou <nils.saadi@etu.univ-amu.fr>
2023-03-22 15:11:08 +0100

    vérification d'email avec un lien

--- Controllers/CheckemailController.php
+++ Controllers/CheckemailController.php
@@ -4,7 +4,7 @@ final class CheckemailController{
 
     public function defaultAction(Array $A_parametres = null, Array $A_postParams = null): void{
         if(!Users::checkIfExistsByEmail($A_postParams['email'])){
-            Checkemail::sendMail($A_postParams['email'], $A_postParams['id']);
+            Checkemail::sendMail($A_postParams);
             View::show("checkemail/form", $A_postParams);
         }
         header("location: /signup");
@@ -12,19 +12,19 @@ final class CheckemailController{
     }
 
     public function steptwoAction(Array $A_parametres = null, Array $A_postParams = null){
-        $A_checkemail = Checkemail::selectById($A_postParams["id"]);
+        $A_checkemail = Checkemail::selectByToken($A_parametres[0]);
         $I_date = mktime(date("H"), date("i")-10, date("s"), date("m"), date("d"), date("Y"));
         $S_date = date("d-M-Y H:i",$I_date);
-        if ($A_postParams['token'] == $A_checkemail['token'] && $A_checkemail['creation_date'] < $S_date){
-            unset($A_postParams['token']);
-            $A_postParams['password'] = hash('sha512', $A_postParams['password'].$A_postParams['id']);
-            if(Users::create($A_postParams)){
-                Checkemail::deleteByID($A_postParams["id"]);
-                Session::start('user', $A_postParams['id']);
+        if ($A_parametres[0] == $A_checkemail['token'] && $A_checkemail['creation_date'] < $S_date){
+            unset($A_checkemail['token']);
+            unset($A_checkemail['creation_date']);
+            if(Users::create($A_checkemail)){
+                Checkemail::deleteByID($A_checkemail["id"]);
+                Session::start('user', $A_checkemail['id']);
                 header('Location: /home');
                 exit;
             }
         }
-        View::show("checkemail/form", $A_postParams);
+        echo'la fin';
     }
 }
--- Model/Checkemail.php
+++ Model/Checkemail.php
@@ -2,19 +2,28 @@
 
 final class Checkemail extends Model{
 
-    public static function sendmail($S_mail, $S_id){
-        $A_params['token'] = self::genToken();
-        $A_params['id'] = $S_id;
+    public static function selectByToken(string $S_token){
+        $O_con = Connection::initConnection();
+        $S_stmnt = "SELECT * FROM checkemail WHERE token = :token ";
+        $P_sth = $O_con->prepare($S_stmnt);
+        $P_sth -> bindValue(":token", $S_token, PDO::PARAM_STR);
+        $P_sth->execute();
+        return $P_sth->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public static function sendmail($A_params){
+        $A_params['token'] = self::genToken($A_params['id']);
         $A_params['creation_date'] = date("d-M-Y H:i");
+        $A_params['password'] = hash('sha512', $A_params['password'].$A_params['id']);
         if(self::create($A_params)){
             $A_mailContent['subject'] = "Vérification d'email";
-            $A_mailContent['body'] = "Voici votre token valable pendant 10min : ".$A_params['token'];
-            Mailer::sendMail($S_mail, $A_mailContent);
+            $A_mailContent['body'] = "Voici votre token valable pendant 10min : locahost/checkemail/steptwo/".$A_params['token'];
+            Mailer::sendMail($A_params['email'], $A_mailContent);
         }
     }
 
-    public static function genToken(){
-        return rand(100000, 999999);
+    public static function genToken($S_id){
+       return hash('sha512', rand(100000, 999999).$S_id);
     }
 
 }
