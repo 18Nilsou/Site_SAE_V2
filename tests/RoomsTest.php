3143553cac647668385559384823a937f2b1ffff
Amaury Vial <82141847+amaury-vial@users.noreply.github.com>
2023-03-29 15:39:15 +0200

    ajout RoomsTest

---
+++ tests/RoomsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once '../Core/AutoLoad.php';
+use PHPUnit\Framework\TestCase;
+
+class RoomsTest extends TestCase
+{
+    public function testUniqueId()
+    {
+        $this->assertEquals(strlen(Rooms::uniqueId()), 6);
+    }
+
+    public function testGetStatus()
+    {
+        // Arrange
+        date_default_timezone_set('Europe/Paris');
+        $start_date = date("Y-m-d H:i:s", strtotime('-1 day'));
+        $end_date = date("Y-m-d H:i:s", strtotime('+1 day'));
+
+        // Act
+        $status = Rooms::getStatus($start_date, $end_date);
+
+        // Assert
+        $this->assertEquals('Active', $status);
+    }
+
+}
