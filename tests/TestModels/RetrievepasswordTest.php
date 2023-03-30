83977b69168bf900322e076a78bbd92a1c73c691
Amaury Vial <82141847+amaury-vial@users.noreply.github.com>
2023-03-30 15:27:32 +0200

    correction

---
+++ tests/TestModels/RetrievepasswordTest.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once '../../Core/AutoLoad.php';
+
+use PHPUnit\Framework\TestCase;
+
+class RetrievepasswordTest extends TestCase
+{
+    public function testGenToken()
+    {
+        $I_token = RetrievePassword::genToken();
+        $this->assertTrue(is_int($I_token));
+        $this->assertTrue($I_token > 100000 && $I_token < 999999);
+    }
+
+}
