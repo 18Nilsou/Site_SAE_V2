d15e81275bb8d75415f2f3273f9df77829a09c25
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2023-03-31 23:37:04 +0200

    Ajout des pages de l'application et modification de la page d'accueil

--- Views/home/home.php
+++ Views/home/home.php
@@ -10,12 +10,28 @@
             <h4>Page d'accueil</h4>
         </section>
         <section class='page'>
-            <img src='/static/pictures/screenshots/screenshot1.png' alt="Page d'accueil">
+            <img src='/static/pictures/screenshots/screenshot2.png' alt="Page de connexion">
             <h4>Page de connexion</h4>
         </section>
         <section class='page'>
-            <img src='/static/pictures/screenshots/screenshot1.png' alt="Page d'accueil">
-            <h4>Page de jeu</h4>
+            <img src='/static/pictures/screenshots/screenshot3.png' alt="Page menu d'apprentissage">
+            <h4>Page menu d'apprentissage</h4>
+        </section>
+        <section class='page'>
+            <img src='/static/pictures/screenshots/screenshot4.png' alt="Page apprentissage">
+            <h4>Page apprentissage</h4>
+        </section>
+        <section class='page'>
+            <img src='/static/pictures/screenshots/screenshot5.png' alt="Page avec chat global">
+            <h4>Chat global intégré</h4>
+        </section>
+        <section class='page'>
+            <img src='/static/pictures/screenshots/screenshot6.png' alt="Page jeu">
+            <h4>Page jeu</h4>
+        </section>
+        <section class='page'>
+            <img src='/static/pictures/screenshots/screenshot7.png' alt="Page multijoueur">
+            <h4>Page multijoueur</h4>
         </section>
     </section>
 </section>
@@ -40,9 +56,19 @@
     <section class='how-to-use'>
         <h3 id='how-to-title'>3. Java</h3>
         <section>
-            <p>Pour lancer l'application, vous devez nécessairement avoir Java 17 ou une version ultérieure d'installée.</p>
+            <p>Pour lancer l'application, vous devez nécessairement avoir Java 17 ou une version ultérieure d'installée.
+             Le JDK doit contenir obligatoirement JavaFx pour que l'application s'exécute, téléchargez la version pour votre ordinateur
+            ci-dessous !</p>
         </section>
-        <a class='btn-how-to' href='https://jdk.java.net/' target="_blank"><h3 class='btn-how-to-text'>Site de java</h3></a>
+        <a class='btn-how-to' href='https://bell-sw.com/pages/downloads/' target="_blank"><h3 class='btn-how-to-text'>Site du JDK de Liberica</h3></a>
+    </section>
+    <section class='how-to-use'>
+        <h3 id='how-to-title'>4. Lancement</h3>
+        <section>
+            <p>Une fois java installé, vous pouvez double-cliquer sur le fichier .jar téléchargé afin de lancer l'application.
+            Sinon, vous pouvez entrer la commande "java -jar FindTheBreachApp.jar" dans votre terminal où est présent le fichier.</p>
+        </section>
+        <a class='btn-how-to' href='https://bell-sw.com/pages/downloads/' target="_blank"><h3 class='btn-how-to-text'>Site du JDK de Liberica</h3></a>
     </section>
 </section>
 
