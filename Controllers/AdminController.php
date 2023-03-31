9f654c0413f2cf81b42b0d704414a7dcce39afc9
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2023-03-31 15:22:13 +0200

    PhpDocs AdminController

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -2,6 +2,11 @@
 
 final class AdminController
 {
+    /**
+     * Redirects the user to the home page if the status of the session is not "admin",
+     * else shows the admin page with the list of questions for solo part
+     * @return void
+     */
     public function defaultAction(): void
     {
         if(Session::getSession()['status'] != 'admin') {
@@ -15,6 +20,13 @@ final class AdminController
         View::show("admin/solo", $A_questions);
     }
 
+    /**
+     *
+     * Redirects to the home page if the user is not an admin,
+     * else shows the admin multiplayer page with the list of rooms and create rooms form
+     *
+     * @return void
+     */
     public function multiplayerAction(): void
     {
         if(Session::getSession()['status'] != 'admin') {
@@ -25,6 +37,12 @@ final class AdminController
         View::show("admin/multiplayer", Rooms::selectRoomsByAdmin(Session::getSession()['id']));
     }
 
+    /**
+     * Redirects to the home page if the user is not an admin,
+     * else shows the users management page
+     *
+     * @return void
+     */
     public function usersAction(): void
     {
         if(Session::getSession()['status'] != 'admin') {
@@ -35,6 +53,14 @@ final class AdminController
         View::show("admin/users");
     }
 
+    /**
+     * Redirects to the home page if the user is not an admin,
+     * else creates a room with the parameters given in the form
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function createroomAction(Array $A_parametres = null, Array $A_postParams = null): void
     {
         if(Session::getSession()['status'] != 'admin') {
@@ -47,6 +73,14 @@ final class AdminController
         header('Location: /admin/multiplayer');
     }
 
+    /**
+     * Redirects to the home page if the user is not the admin of the room,
+     * else deletes the room with the given id
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function deleteroombyidAction(Array $A_parametres = null, Array $A_postParams = null):void {
         if (Rooms::selectById($A_parametres[0])['admin_id'] == Session::getSession()['id']) {
             Rooms::deleteByID($A_parametres[0]);
@@ -55,11 +89,19 @@ final class AdminController
         exit;
     }
 
+    /**
+     * Redirects to the home page if the user is not the admin of the room,
+     * else shows the modify room page with the room's information and the list of questions
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function modifyroomAction(Array $A_parametres = null, Array $A_postParams = null): void
     {
         $A_room = Rooms::selectById($A_parametres[0]);
         if ($A_room['admin_id'] != Session::getSession()['id']) {
-            header('Location: /admin/multiplayer');
+            header('Location: /home');
             exit;
         }
         View::show("admin/admin-nav");
@@ -79,11 +121,19 @@ final class AdminController
         }
     }
 
+    /**
+     * Redirects to the home page if the user is not the admin of the room,
+     * else modifies the room with the given id with the dates given in the form
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function modifyroomdatesAction(Array $A_parametres = null, Array $A_postParams = null): void
     {
         $A_room = Rooms::selectById($A_postParams['roomId']);
         if ($A_room['admin_id'] != Session::getSession()['id']) {
-            header('Location: /admin/multiplayer');
+            header('Location: /home');
             exit;
         }
         $A_room['start_date'] = $A_postParams['start_date'];
@@ -93,6 +143,14 @@ final class AdminController
         exit;
     }
 
+    /**
+     * Redirects to the home page if the user is not the admin of the room,
+     * else delete user from the room's whitelist with the given id
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function blacklistuserAction(Array $A_parametres = null, Array $A_postParams = null) {
         $A_room = Rooms::selectById($A_postParams['roomId']);
         if ($A_room['admin_id'] != Session::getSession()['id']) {
@@ -104,6 +162,15 @@ final class AdminController
         exit;
     }
 
+    /**
+     * Redirects to the home page if the user is not the admin of the room,
+     * else invites users to the room with the given id with the emails given in the form
+     * and adds them to the room's whitelist
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function inviteusersAction($A_parametres = null, Array $A_postParams = null) {
         $A_unSignedUsers = array();
         $A_room = Rooms::selectById($A_postParams['roomId']);
@@ -129,14 +196,30 @@ final class AdminController
         $this->modifyroomAction($A_parametres, $A_postParams);
     }
 
+    /**
+     * Redirects to the home page if the user is not an admin,
+     * else deletes or modify the question with the given id with the datas given in the form
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function modifyOrDeleteQuestionAction(Array $A_parametres = null, Array $A_postParams = null) : void{
         if (!Session::isAdmin()) {
-            header("location: /signin");
+            header("location: /home");
         }
         Questions::form($A_postParams);
         header("location: /admin");
     }
 
+    /**
+     * Redirects to the home page if the user is not the admin of the room,
+     * else deletes or modify the question with the given id with the datas given in the form
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function modifyOrDeleteQuestionRoomAction(Array $A_parametres = null, Array $A_postParams = null) : void{
         $A_room = Rooms::selectById($A_postParams['room_id']);
         if ($A_room['admin_id'] != Session::getSession()['id']) {
@@ -148,15 +231,31 @@ final class AdminController
         exit;
     }
 
+    /**
+     * Redirects to the home page if the user is not an admin,
+     * else adds a question with the datas given in the form
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function addQuestionAction(Array $A_parametres = null, Array $A_postParams = null) : void{
         if (!Session::isAdmin()) {
-            header("location: /signin");
+            header("location: /home");
             exit;
         }
         Questions::add($A_postParams);
         header("location: /admin");
     }
 
+    /**
+     * Redirects to the home page if the user is not the admin of the room,
+     * else adds a question in the room with the datas given in the form
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function addRoomQuestionAction(Array $A_parametres = null, Array $A_postParams = null) : void{
         $A_room = Rooms::selectById($A_postParams['room_id']);
         if ($A_room['admin_id'] != Session::getSession()['id']) {
@@ -168,27 +267,51 @@ final class AdminController
         exit;
     }
 
+    /**
+     * Redirects to the home page if the user is not an admin,
+     * else add an admin with the datas given in the form
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function addAdminAction(Array $A_parametres = null, Array $A_postParams = null) : void{
         if (!Session::isAdmin()) {
-            header("location: /signin");
+            header("location: /home");
             exit;
         }
         Admins::create($A_postParams);
         header("location: /admin/users");
     }
 
+    /**
+     * Redirects to the home page if the user is not an admin,
+     * else deletes the admin with the given id
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function deleteAdminAction(Array $A_parametres = null, Array $A_postParams = null) : void{
         if (!Session::isAdmin()) {
-            header("location: /signin");
+            header("location: /home");
             exit;
         }
         Admins::deleteByID($A_postParams['id']);
         header("location: /admin/users");
     }
 
+    /**
+     * Redirects to the home page if the user is not an admin,
+     * else deletes the user with the given id
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function deleteUserAction(Array $A_parametres = null, Array $A_postParams = null) : void{
         if (!Session::isAdmin()) {
-            header("location: /signin");
+            header("location: /home");
             exit;
         }
         Admins::deleteByID($A_postParams['id']);
@@ -196,20 +319,52 @@ final class AdminController
         header("location: /admin/users");
     }
 
+    /**
+     * Redirects to the home page if the user is not an admin,
+     * else generates a csv file with the scores requested and download it
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function getScoreAction(Array $A_parametres = null, Array $A_postParams = null) : void{
         if (!Session::isAdmin()) {
-            header("location: /signin");
+            header("location: /home");
             exit;
         }
         Files::score(Rooms::getScore($A_postParams));
         Files::download("score.txt","Files/score.txt");
     }
 
+    /**
+     * Redirects to the home page if the user is not an admin,
+     * else generates an example of csv file and download it
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function getfilequestionsAction(Array $A_parametres = null, Array $A_postParams = null) : void{
+        if (!Session::isAdmin()) {
+            header("location: /home");
+            exit;
+        }
         Files::download("questions.csv","Files/questions.csv");
     }
 
+    /**
+     * Redirects to the home page if the user is not an admin,
+     * else adds the questions with the csv file given in the form
+     *
+     * @param array|null $A_parametres
+     * @param array|null $A_postParams
+     * @return void
+     */
     public function getquestionfromfileAction(Array $A_parametres = null, Array $A_postParams = null) : void{
+        if (!Session::isAdmin()) {
+            header("location: /home");
+            exit;
+        }
         $S_origin = $_FILES['file']['tmp_name'];
         $S_path = 'Files/ '.$_FILES['file']['name'];
         move_uploaded_file($S_origin,$S_path);
