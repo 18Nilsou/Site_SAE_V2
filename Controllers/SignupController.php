a1e7b15251a48aa2f6a079d4572e1d75588ee88c
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2023-04-01 23:54:11 +0200

    Résolution bug inscription

--- Controllers/SignupController.php
+++ Controllers/SignupController.php
@@ -45,12 +45,11 @@ final class SignupController{
     public function registerAction(Array $A_parametres = null, Array $A_postParams = null):void{
         $A_postParams['password'] = hash('sha512', $A_postParams['password'].$A_postParams['id']);
 
-        if (Users::checkIfExistsById($A_postParams['id'])){
-            header("location: /signup/formError");
-            exit;
-        } else{
+        if(!Users::checkIfExistsById($A_postParams['id'])){
             Checkemail::sendMail($A_postParams['id'], $A_postParams['email']);
             View::show("checkemail/form",$A_postParams);
+        }else{
+            header("location: /signup/formError");
             exit;
         }
     }
@@ -65,13 +64,11 @@ final class SignupController{
      * @return void
      */
     public function checkemailAction(Array $A_parametres = null, Array $A_postParams = null){
-
         $A_checkemail = Checkemail::selectById($A_postParams['id']);
         $I_date = mktime(date("H"), date("i")-10, date("s"), date("m"), date("d"), date("Y"));
         $S_date = date("d-M-Y H:i",$I_date);
-        if ($A_postParams['token'] == $A_checkemail['token'] && $A_checkemail['creation_date'] < $S_date){
+        if ($A_postParams['token'] == $A_checkemail['token'] && $A_checkemail['creation_date'] > $S_date){
             unset($A_postParams['token']);
-            var_dump($A_postParams);
             if(Users::create($A_postParams)){
                 Checkemail::deleteByID($A_postParams['id']);
                 Session::start('user', $A_postParams['id']);
