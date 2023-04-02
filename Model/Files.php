d09509b5d213c569f07b7bfade45992b17ec360b
18Nilsou <nils.saadi@etu.univ-amu.fr>
2023-04-02 13:36:06 +0200

    recupe les scores

--- Model/Files.php
+++ Model/Files.php
@@ -31,7 +31,7 @@ final class Files{
     public static function download($S_file, $S_dir){
         header('Content-Description: File Transfer');
         header('Content-Type: application/txt');
-        header('Content-Disposition: attachment; filename=listequestion.csv');
+        header('Content-Disposition: attachment; filename='.$S_file);
         header('Expires: 0');
         header('Cache-Control: must-revalidate');
         header('Pragma: public');
